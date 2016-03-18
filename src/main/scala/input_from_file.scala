35b8fedeaf880b511f78b630820f862475a2d5ae
Ao <aoliu@umass.edu>
2016-03-18 00:00:51 -0400

    convert json file to Cubbie and to DBObject

--- src/main/scala/input_from_file.scala
+++ src/main/scala/input_from_file.scala
@@ -6,33 +6,35 @@ import cc.factorie.db.mongo.MongoCubbieConverter
 import cc.factorie.util.Cubbie
 import org.bson._
 import com.mongodb._
+import scala.collection.mutable
 import scala.util.parsing.json.JSON
 
 import scala.io._
 import cc.factorie.app.nlp.lemma.WordNetLemmatizer
-import cc.factorie.app.nlp.{Sentence, DocumentAnnotationPipeline, Document}
+import cc.factorie.app.nlp.{DocumentStore, Sentence, DocumentAnnotationPipeline, Document}
 import cc.factorie.app.nlp.pos.OntonotesForwardPosTagger
 import cc.factorie.app.nlp.segment.{DeterministicSentenceSegmenter, DeterministicNormalizingTokenizer}
 
 object Adapter{
     def main(args: Array[String]) {
-        val mongoClient = new MongoClient("localhost", 27972)
-        val mongoDB = mongoClient.getDB("MyTest")
-        val mongoCollection = mongoDB.getCollection("MyCollection")
 
-        mongoCollection.drop()
-        //println(mongoCollection.getCount())
+        val mongo = new MongoClient()
+        val db = mongo.getDB("Test")
+        val collection = db.getCollection("documents")
+        
+        collection.drop()
+        println(collection.getCount())
 
-        val myMap = collection.mutable.Map[String, Any]()
+        val myMap = mutable.Map[String, Any]()
         myMap.put("Ao", "Liu")
         myMap.put("Emma", "Strubell")
         myMap.put("Andrew", "McCallum")
         val cubbie : Cubbie = new Cubbie(myMap)
         val myObject = MongoCubbieConverter.eagerDBO(cubbie)
 
-        //mongoCollection.remove(myObject)
-        mongoCollection.insert(myObject)
-        //println(mongoCollection.findOne())
+        //collection.remove(myObject)
+        collection.insert(myObject)
+        //println(collection.findOne())
 
         val example : DBObject  = new BasicDBObject
         example.put("name", "MongoDB")
@@ -44,21 +46,30 @@ object Adapter{
 
         example.put("info", info)
 
-        //mongoCollection.remove(example)
-        mongoCollection.insert(example)
-        //println(mongoCollection.findOne())
+        //collection.remove(example)
+        collection.insert(example)
+        //println(collection.findOne())
 
+        /*
         var o : DBObject = null
-        while((o = mongoCollection.findOne()) != null &&  mongoCollection.getCount() > 0){
-            mongoCollection.remove(o)
+        while((o = collection.findOne()) != null &&  collection.getCount() > 0){
+            collection.remove(o)
             println(o)
         }
+        */
 
-        val jsonFile = Source.fromFile("src/main/resources/input.json")
-        val jsonContent = jsonFile.mkString
-        val jsonMap = JSON.parseFull(jsonContent).get
-        println(jsonMap)
+        val file = Source.fromFile("src/main/resources/input.json")
+        val content = file.mkString
+        val map = mutable.Map[String, Any]() ++ JSON.parseFull(content).get.asInstanceOf[Map[String, Any]]
+        val mongoCotent = MongoCubbieConverter.toMongo(content)
+        val newCubbie : Cubbie = new Cubbie(map)
+        val dbo = MongoCubbieConverter.eagerDBO(newCubbie)
+        val c = collection.findOne()
 
+        println(dbo)
+        val doc = new DocumentStore(db.getName)
+        //doc += new Document(content)
+        
         /*
         val doc = new Document(content)
         val annotators = Seq(DeterministicNormalizingTokenizer, DeterministicSentenceSegmenter, OntonotesForwardPosTagger, WordNetLemmatizer)
