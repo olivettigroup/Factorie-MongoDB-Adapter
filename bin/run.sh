10cef90348b2d64b23a8a604b407b95de746995f
Emma Strubell <emma.strubell@gmail.com>
2016-06-02 19:27:37 -0600

    script

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,12 @@
 
 MEMORY=10g
 
-$FAC_ADAPTER_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.lffi.parse.TransitionBasedParserTrainer --option=value
+$FAC_ADAPTER_ROOT/bin/run_class.sh -Xmx$MEMORY adapter.Adapter \
+--inputDB predsynth \
+--input-collection paragraphs \
+--port-num 27017 \
+--port-name localhost \
+--outputDB outputDB \
+--output-collection outputCollection \
+--maps-dir /home/umaguest/intmaps/standard-psr-chen-collobert \
+--model /home/umaguest/model-ptb-standard-psr-chen-collobert.torch.hd5
