20517806e65fa0559181c1e9a73e17c8fc27c999
Emma Strubell <emma.strubell@gmail.com>
2016-06-02 19:18:58 -0600

    fix scripts

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,4 @@
 
 MEMORY=10g
 
-$SPECIFIC_PROJECT_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.lffi.parse.TransitionBasedParserTrainer --option=value
+$FAC_ADAPTER_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.lffi.parse.TransitionBasedParserTrainer --option=value
